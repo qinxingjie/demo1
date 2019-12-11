a6ca46d73b973d2a7e4e2edc74f0aed9c9580599
Joey <qinxingjie@zork.com.cn>
2019-12-11 11:24:01 +0800

    date util commit

--- src/main/java/com/example/demo/util/SqlUtil.java
+++ src/main/java/com/example/demo/util/SqlUtil.java
@@ -1,8 +1,19 @@
 package com.example.demo.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author qinxingjie
  * @date 2019/12/10 16:26
  */
 public class SqlUtil {
+    public static String getCurrentDate(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss"));
+    }
+
+    public static void main(String[] args){
+        System.out.println(getCurrentDate());
+    }
 }
